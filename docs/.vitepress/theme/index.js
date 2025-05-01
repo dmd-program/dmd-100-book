4020149bbd114d22d1734edd01838bcdaf2815d6
Michael Collins <michael-collins@users.noreply.github.com>
2025-04-30 21:35:40 -0400

    fixed video embed

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,12 +3,15 @@ import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 import { h } from 'vue'
 import LicenseFooter from './components/LicenseFooter.vue'
+import VideoEmbed from './components/VideoEmbed.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // Register the license component so it can be used in markdown
     app.component('LicenseFooter', LicenseFooter)
+    // Register the VideoEmbed component for video embeds
+    app.component('VideoEmbed', VideoEmbed)
   },
   Layout() {
     return h(DefaultTheme.Layout, null, {
